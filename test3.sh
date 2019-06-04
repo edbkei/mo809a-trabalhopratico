2c3b3e4ff9f62766df139ce82f5b160fa3294180
edbkei <eduardoito2010@yahoo.com>
2019-06-04 12:25:46 -0300

    Add files via upload

---
+++ test3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Autor: Eduardo S. Ito <e159086@g.unicamp.br>
+#Data: 23/05/2019
+#Nota: script para rodar simulacao de MO809A OMNETPP - T. de Sistema Distribuído - 2019
+#Rodar: ./mo809a2019omnet.py
+
+# * Combinações para o comando run_mo809b.sh
+#
+# ./run_mo809b.sh {p1} [p2] [p3]
+#
+
+
+#Vai para diretorio ns-3.29
+cd /home/mo809a
+python mo809a2019omnet.py
+
+#EOF
