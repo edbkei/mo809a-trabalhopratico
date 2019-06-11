23f7f9f59fef5367846628ea0a32653ab483ba99
edbkei <eduardoito2010@yahoo.com>
2019-06-11 09:15:13 -0300

    Add files via upload

---
+++ test4.sh
@@ -0,0 +1,7 @@
+# execute ./runtrab.sh p1 p2 p3
+# p1 number of interactions for distance
+# p2 simulation time in seconds
+# p3 distance to Rx in meters
+#
+cd ~/repos/ns-3-allinone/ns-3-dev/scratch
+./runtrab.sh p1 p2 p3
