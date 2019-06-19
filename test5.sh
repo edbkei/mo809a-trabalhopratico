edf12f8d11506663fb9cfe93fb3309f17dc7e4fd
edbkei <eduardoito2010@yahoo.com>
2019-06-19 20:49:32 -0300

    Add files via upload

---
+++ test5.sh
@@ -0,0 +1,9 @@
+# to Run IEEE 802.11a OfdmRate12Mbps
+# execute ./runtrabb.sh p1 p2 p3
+# p1 number of interactions for distance
+# p2 simulation time in seconds
+# p3 distance to Rx in meters
+#
+cd repos/ns-3-allinone/ns-3-dev/scratch
+./runtrabb.sh 14 100 100
+
