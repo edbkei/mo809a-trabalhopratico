33f3d1326106aa7ff96e43e12313447f88eacc37
edbkei <eduardoito2010@yahoo.com>
2019-06-27 14:46:38 -0300

    Add files via upload

---
+++ test6.sh
@@ -0,0 +1,9 @@
+# to Run IEEE 802.11b base wirelesanimation.cc
+# execute ./runtrabanim.sh p1 p2 p3
+# p1 number of interactions for distance
+# p2 simulation time in seconds
+# p3 distance to Rx in meters
+#
+cd repos/ns-3-allinone/ns-3-dev/scratch
+./runtrabanim.sh 14 100 25
+
